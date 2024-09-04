1b532608cef244b55143a356821dd3585c3a9d6f
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-09-03 21:25:21 -0400

    style: fix linting and formatting

--- backend/app/core/tests/test_models.py
+++ backend/app/core/tests/test_models.py
@@ -1,17 +1,19 @@
 """
 Tests for models
 """
+
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
+
 class ModelTests(TestCase):
-  def test_create_user_with_email_successfully(self):
-    email = 'test@example.com'
-    password = '123456'
-    user = get_user_model().objects.create_user(
-      email=email,
-      password=password,
-    )
-    
-    self.assertEqual(user.email, email)
-    self.assertTrue(user.check_password(password))
+    def test_create_user_with_email_successfully(self):
+        email = "test@example.com"
+        password = "123456"
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password,
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
