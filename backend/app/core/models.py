feb50e263a324d7a016753518553f73d2fd71c03
Hungle2911 <122929293+Hungle2911@users.noreply.github.com>
2024-09-12 18:29:51 -0400

    feat: add recipe model

--- backend/app/core/models.py
+++ backend/app/core/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.conf import settings
 from django.contrib.auth.models import (
     AbstractBaseUser,
     BaseUserManager,
@@ -10,7 +11,7 @@ from django.contrib.auth.models import (
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
         if not email:
-            raise ValueError('User must have an email address')
+            raise ValueError("User must have an email address")
         user = self.model(email=self.normalize_email(email), **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
@@ -35,3 +36,14 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
+
+
+class Recipe(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    time_minutes = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.title
--- backend/app/core/tests/test_models.py
+++ backend/app/core/tests/test_models.py
@@ -2,6 +2,8 @@
 Tests for models
 """
 
+from decimal import Decimal
+from core import models
 from django.test import TestCase
 from django.contrib.auth import get_user_model
 
@@ -41,3 +43,18 @@ class ModelTests(TestCase):
 
         self.assertTrue(user.is_superuser)
         self.assertTrue(user.is_staff)
+
+    def test_create_recipe(self):
+        """Test creating a new recipe"""
+        user = get_user_model().objects.create_user(
+            "test@example.com",
+            "test123",
+        )
+        recipe = models.Recipe.objects.create(
+            user=user,
+            title="Steak and mushroom sauce",
+            time_minutes=5,
+            price=Decimal("5.00"),
+            description="This is a test description",
+        )
+        self.assertEqual(str(recipe), recipe.title)
